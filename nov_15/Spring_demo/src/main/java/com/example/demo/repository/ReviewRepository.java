a73844ce2ba0d530eb1ebff879153939ea55fe6f
Sreenidhiangadi <134719947+Sreenidhiangadi@users.noreply.github.com>
2025-11-30 16:36:42 +0530

    Delete  directory

--- nov_15/Spring_demo/src/main/java/com/example/demo/repository/ReviewRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.request.Review;
-
-public interface ReviewRepository extends JpaRepository<Review, Integer> {
-}
