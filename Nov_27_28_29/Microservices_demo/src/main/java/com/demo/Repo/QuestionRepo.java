d75b3efd4ca5fcf8b268657a12335c8e0ccf0fdd
Sreenidhi Angadi <angadisreenidhi@gmail.com>
2025-12-06 15:22:34 +0530

    Revert "Deleted directory"
    This reverts commit 529e9f31da8b6af99970e5e978b9ce606b1285b6.

---
+++ Nov_27_28_29/Microservices_demo/src/main/java/com/demo/Repo/QuestionRepo.java
@@ -0,0 +1,22 @@
+package com.demo.Repo;
+
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.demo.entity.Question;
+
+@Repository
+public interface QuestionRepo extends JpaRepository<Question, Integer> {
+
+    List<Question> findByCategory(String category);
+
+    @Query(value = "SELECT * FROM question WHERE category = ?1 ORDER BY RAND()", 
+    	       nativeQuery = true)
+    	List<Question> findRandomQuestionsByCategory(String category, Pageable pageable);
+
+
+}
---
+++ Nov_27_28_29/Microservices_demo/src/main/java/com/demo/service/QuestionService.java
@@ -0,0 +1,95 @@
+package com.demo.service;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.demo.Repo.QuestionRepo;
+import com.demo.entity.Question;
+import com.demo.entity.QuestionWrapper;
+import com.demo.entity.Response;
+
+@Service
+public class QuestionService {
+    @Autowired
+    QuestionRepo questionRepo;
+
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questionRepo.findAll(), HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionRepo.findByCategory(category),HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+
+    }
+
+    public ResponseEntity<String> addQuestion(Question question) {
+        questionRepo.save(question);
+        return new ResponseEntity<>("success",HttpStatus.CREATED);
+    }
+
+  
+
+    public List<Integer> getQuestionsForQuiz(String categoryName, Integer numQuestions) {
+
+        Pageable limit = PageRequest.of(0, numQuestions);
+
+        List<Question> questions = questionRepo.findRandomQuestionsByCategory(categoryName, limit);
+
+        return questions.stream().map(Question::getId).collect(Collectors.toList());
+    }
+
+
+    public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(List<Integer> questionIds) {
+        List<QuestionWrapper> wrappers = new ArrayList<>();
+        List<Question> questions = new ArrayList<>();
+
+        for(Integer id : questionIds){
+            questions.add(questionRepo.findById(id).get());
+        }
+
+        for(Question question : questions){
+            QuestionWrapper wrapper = new QuestionWrapper();
+            wrapper.setId(question.getId());
+            wrapper.setQuestionTitle(question.getQuestionTitle());
+            wrapper.setOption1(question.getOption1());
+            wrapper.setOption2(question.getOption2());
+            wrapper.setOption3(question.getOption3());
+            wrapper.setOption4(question.getOption4());
+            wrappers.add(wrapper);
+        }
+
+        return new ResponseEntity<>(wrappers, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Integer> getScore(List<Response> responses) {
+
+
+        int right = 0;
+
+        for(Response response : responses){
+            Question question = questionRepo.findById(response.getId()).get();
+            if(response.getResponse().equals(question.getRightAnswer()))
+                right++;
+        }
+        return new ResponseEntity<>(right, HttpStatus.OK);
+    }
+}
